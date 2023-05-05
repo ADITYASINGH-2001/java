7e99f585d2dc041e3ec53f6d2b9ecb82d0d6a8d1
ADITYASINGH-2001 <119028526+ADITYASINGH-2001@users.noreply.github.com>
2023-05-06 02:08:33 +0530

    Add files via upload

---
+++ sumproquo.java
@@ -0,0 +1,15 @@
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		// your code goes here
+		int a = -50;
+		int b = 40;
+		int sum = -50+40;
+		int product = -50*40;
+		int quotient = -50/40;
+		System.out.println(sum);
+		System.out.println(product);	
+		System.out.print(quotient);	
+	}
+}
