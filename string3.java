585792dcd04e9f0f73e39c041d57320a94973e61
ADITYASINGH-2001 <119028526+ADITYASINGH-2001@users.noreply.github.com>
2023-05-23 03:03:08 +0530

    Add files via upload

---
+++ string3.java
@@ -0,0 +1,11 @@
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		// your code goes here
+        String txt = "NumeroTres";
+        System.out.println("The length of the word is: " + txt.length());
+        System.out.println("The text in lower case is: " + txt.toLowerCase());
+        System.out.println("The text in upper case is: " + txt.toUpperCase());
+	}
+}
