a51d4645ca98c5da02c204d9b5112fb6c34149a4
ADITYASINGH-2001 <119028526+ADITYASINGH-2001@users.noreply.github.com>
2023-05-24 01:25:26 +0530

    Add files via upload

---
+++ string2.java
@@ -0,0 +1,10 @@
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		// your code goes here
+		String x = "Hello";
+        String y = "world";
+        System.out.print(x+ " " + y);
+	}
+}
