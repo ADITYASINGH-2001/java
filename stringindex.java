9aa57b305bad4da47e94e42bbe627c5d6ff09091
ADITYASINGH-2001 <119028526+ADITYASINGH-2001@users.noreply.github.com>
2023-05-24 01:25:05 +0530

    Add files via upload

---
+++ stringindex.java
@@ -0,0 +1,10 @@
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		// your code goes here
+		String txt = "Please find chef";
+        System.out.println(txt.indexOf("ease"));
+        System.out.println(txt.indexOf("chef"));
+	}
+}
