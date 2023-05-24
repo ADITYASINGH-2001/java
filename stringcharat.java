db42a6cc2289ff1d7d61cc25bcf61c366fc9dc22
ADITYASINGH-2001 <119028526+ADITYASINGH-2001@users.noreply.github.com>
2023-05-24 23:37:54 +0530

    Add files via upload

---
+++ stringcharat.java
@@ -0,0 +1,10 @@
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		// your code goes here
+		String myString = "Programming";
+        System.out.println(myString.charAt(2));
+        System.out.print(myString.charAt(4));
+	}
+}
