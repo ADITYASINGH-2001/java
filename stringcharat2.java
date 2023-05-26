49042decec236fdeab3f12819b6384bccc8a4177
ADITYASINGH-2001 <119028526+ADITYASINGH-2001@users.noreply.github.com>
2023-05-26 16:47:01 +0530

    Add files via upload

---
+++ stringcharat2.java
@@ -0,0 +1,13 @@
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		// your code goes here
+		String var = "String";
+		System.out.print(var.charAt(0));
+		System.out.print(var.charAt(1));
+        System.out.println(var.charAt(2));
+        //substring is a method used to extract a portion of the given string
+        System.out.print(var.substring(0,3));
+	}
+}
