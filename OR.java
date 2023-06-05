28aad818e5cddae1fea022c99ea35fdea9db3dcf
ADITYASINGH-2001 <119028526+ADITYASINGH-2001@users.noreply.github.com>
2023-06-05 23:55:35 +0530

    Add files via upload

---
+++ OR.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		// your code goes here
+		Scanner read = new Scanner(System.in);
+		int z = read.nextInt();
+		int x = read.nextInt();
+		int c = read.nextInt();
+        if (c>x || c>z){
+            System.out.println("PASS");
+        }
+        else{
+            System.out.println("FAIL");
+        }
+	}
+}
+
