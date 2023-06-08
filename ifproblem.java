250d26212b5a5b95cb62d88498670aa98a0469c2
ADITYASINGH-2001 <119028526+ADITYASINGH-2001@users.noreply.github.com>
2023-06-08 21:53:54 +0530

    Add files via upload

---
+++ ifproblem.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		// your code goes here
+		Scanner read = new Scanner(System.in);
+		int a = read.nextInt();
+		int b = read.nextInt();
+		int c = read.nextInt();
+		if(a >= c){
+		    System.out.print("Bravo!");
+		}
+		else{
+		    System.out.print("Try again");
+		}
+		
+	}
+}
