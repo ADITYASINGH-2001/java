c4595c2c05e1385c11f515319cf03cc1514f3ffe
ADITYASINGH-2001 <119028526+ADITYASINGH-2001@users.noreply.github.com>
2023-06-14 23:39:06 +0530

    Add files via upload

---
+++ changingArray.java
@@ -0,0 +1,10 @@
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		// your code goes here
+		String[] mnts = {"January", "February", " December", "April"};
+		mnts[2] = "March";
+		System.out.print(mnts[2]);
+	}
+}
