822fb693561b1323948bbc77791cf7349fb5e447
ADITYASINGH-2001 <119028526+ADITYASINGH-2001@users.noreply.github.com>
2023-06-16 01:46:04 +0530

    Add files via upload

---
+++ DisplayingElements.java
@@ -0,0 +1,10 @@
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		// your code goes here
+		String[] week = {"Monday", "Tuesday", " Wednesday", "Thursday"};
+		System.out.println(week[2]);
+		System.out.print(week[3]);
+	}
+}
