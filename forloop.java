92d5b01d2b95c8f1da110f42d2363a962e573fa3
ADITYASINGH-2001 <119028526+ADITYASINGH-2001@users.noreply.github.com>
2023-06-19 23:10:44 +0530

    Add files via upload

---
+++ forloop.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner read = new Scanner(System.in);
+		int num = read.nextInt();
+		for(int i = 0; i <num; i++){
+		    System.out.println("java");
+		}
+	}
+}
