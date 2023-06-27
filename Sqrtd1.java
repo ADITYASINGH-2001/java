63e3422389ccd96885a7bc18c42ca57331a5bf9f
Aditya Singh <119028526+ADITYASINGH-2001@users.noreply.github.com>
2023-06-28 02:42:15 +0530

    Add files via upload

---
+++ Sqrtd1.java
@@ -0,0 +1,8 @@
+import java.lang.Math;
+class Codechef {
+public static void main(String[] args) {
+int x = 16;
+double sqrtValue = Math.sqrt(x);
+System.out.println("Square root: " + sqrtValue);
+}
+}
