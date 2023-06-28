68c26b174f5d702d64e3b87803d6c4f7f20b43e4
Aditya Singh <119028526+ADITYASINGH-2001@users.noreply.github.com>
2023-06-29 01:54:28 +0530

    Add files via upload

---
+++ numertrdenomtrD2.java
@@ -0,0 +1,8 @@
+class Codechef {
+public static void main(String[] args) {
+int numerator = 20;
+int denominator = 7;
+int remainder = numerator % denominator;
+System.out.println("Remainder: " + remainder);
+}
+}
